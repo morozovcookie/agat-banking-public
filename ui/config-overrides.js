b66b09d8bf613e362d37d33c6706fe1548686844
Konstantin Morozov <morozov.cookie@gmail.com>
2021-10-27 17:48:34 +0300

    ability to customize antd default values

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -1,26 +1,29 @@
-module.exports = (config, env) => {
-    const path = require('path');
+const { override, addLessLoader, fixBabelImports, addWebpackAlias } = require('customize-cra');
+const path = require('path');
 
-    let alias = config.resolve.alias;
-
-    alias = {
-        ...alias,
-
-        '@': path.resolve(__dirname, 'src'),
-        '@assets': path.resolve(__dirname, 'src/assets'),
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@constants': path.resolve(__dirname, 'src/constants'),
-        '@contexts': path.resolve(__dirname, 'src/contexts'),
-        '@hooks': path.resolve(__dirname, 'src/hooks'),
-        '@i18n': path.resolve(__dirname, 'src/i18n'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
-        '@routes': path.resolve(__dirname, 'src/routes'),
-        '@services': path.resolve(__dirname, 'src/services'),
-        '@stores': path.resolve(__dirname, 'src/stores'),
-        '@utils': path.resolve(__dirname, 'src/utils')
-    }
-
-    config.resolve.alias = alias;
-
-    return config;
-}
+module.exports = override(
+    addWebpackAlias({
+        ['@']: path.resolve(__dirname, 'src'),
+        ['@assets']: path.resolve(__dirname, 'src/assets'),
+        ['@components']: path.resolve(__dirname, 'src/components'),
+        ['@constants']: path.resolve(__dirname, 'src/constants'),
+        ['@contexts']: path.resolve(__dirname, 'src/contexts'),
+        ['@hooks']: path.resolve(__dirname, 'src/hooks'),
+        ['@i18n']: path.resolve(__dirname, 'src/i18n'),
+        ['@pages']: path.resolve(__dirname, 'src/pages'),
+        ['@routes']: path.resolve(__dirname, 'src/routes'),
+        ['@services']: path.resolve(__dirname, 'src/services'),
+        ['@stores']: path.resolve(__dirname, 'src/stores'),
+        ['@utils']: path.resolve(__dirname, 'src/utils')
+    }),
+    fixBabelImports('antd', {
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#344468',
+        },
+    })
+)
